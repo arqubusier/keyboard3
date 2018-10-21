35f9c262f38940a0c7e30d0091777309798519cd
Herman Lundkvist <herlu184@student.liu.se>
2018-10-21 21:54:21 +0200

    Cleanup

--- keyboard3.ino
+++ keyboard3.ino
@@ -1,4 +1,12 @@
-/* keyPadHiduino Advanced Example Code
+/* Keyboard3 Keyboard firmware
+   by: Herman Lundkvist
+   date: 2018
+   license: MIT license - feel free to use this code in any way
+   you see fit. If you go on to use it in another project, please
+   keep this license on there.
+
+
+   Started from an example
    by: Jim Lindblom
    date: January 5, 2012
    license: MIT license - feel free to use this code in any way
@@ -11,12 +19,39 @@
 #include "key_codes.h"
 
 
+enum struct KeyState {UP=HIGH, DOWN=LOW};
+
+struct KeyStatus{
+  KeyState state;
+  unsigned long last_change;
+  uint8_t report_key_i;
+};
+
+enum struct KeyType {KEY, MOD, FUN};
+
+union KeyVal{
+  byte key;
+  byte mod;
+};
+
+struct KeySym{
+  KeyType type;
+  KeyVal val;
+
+  /*
+    KeySym(byte val): type(KeyType::KEY){
+    this->val.key=val;
+    }
+  */
+};
+
+
 /** Get the size of a fixed size array in compile-time */
 template <typename T, size_t N>
 constexpr int dim(T(&)[N]){return N;}
 
 template <typename T, size_t N>
-void pin_status(T(& arr)[N])
+void print_array(T(& arr)[N])
 {
   for (size_t i; i < N; i++){
     Serial.print(arr[i]);
@@ -26,53 +61,32 @@ void pin_status(T(& arr)[N])
 }
 
 void sendKey(byte key, byte modifiers = 0);
+
+// Create an empty KeyReport
+KeyReport report = {0};
+const uint8_t KEY_REPORT_KEY_AVAILABLE = 0;
+
 // enable pin for main loop, active low.
 int enable = 9;
-
-
 // Currently in pins correspond to rows
 int in_pins[] {4, 5};
 // Currently out pins correspond to columns
 int out_pins[]  {10, 16};
 
-enum KeyState {KEY_STATE_UP=HIGH, KEY_STATE_DOWN=LOW};
-
 unsigned long debounce_ms = 5;
 const uint8_t KEY_REPORT_DONT_CLEAR_KEY = 255;
-struct KeyStatus{
-  KeyState state;
-  unsigned long last_change;
-  uint8_t report_key_i;
-};
-
-union KeyVal{
-  byte key;
-  byte mod;
-};
-
-struct KeySym{
-  bool is_key;
-  KeyVal val;
-
-  KeySym(byte val): is_key(true){
-    this->val.key=val;
-  }
-};
 
 KeyStatus status_table[dim(in_pins)][dim(out_pins)];
 const size_t layers = 1;
+size_t layer = 0;
 KeySym symbol_table[layers][dim(in_pins)][dim(out_pins)] =
   {
    {
-    {KeySym(KEY_SW_A), KeySym(KEY_SW_B)},
-    {KeySym(KEY_SW_C), KeySym(KEY_SW_D)},
+    {{KeyType::KEY, KEY_SW_A}, {KeyType::KEY, KEY_SW_B}},
+    {{KeyType::KEY, KEY_SW_C}, {KeyType::KEY, KEY_SW_D}},
    },
   };
 
-// Create an empty KeyReport
-KeyReport report = {0};
-const uint8_t KEY_REPORT_KEY_AVAILABLE = 0;
-size_t layer = 0;
 
 void setup()
 {
@@ -98,7 +112,7 @@ void setup()
 
   for (size_t in_i = 0; in_i < dim(in_pins); in_i++){
     for (size_t out_i = 0; out_i < dim(out_pins); out_i++){
-      status_table[in_i][out_i] = {KEY_STATE_UP, 0, KEY_REPORT_DONT_CLEAR_KEY};
+      status_table[in_i][out_i] = {KeyState::UP, 0, KEY_REPORT_DONT_CLEAR_KEY};
     }
   }
 
@@ -127,26 +141,36 @@ void loop()
       int in_pin = in_pins[in_i];
       int in_val = digitalRead(in_pin);
       KeyStatus &status = status_table[in_i][out_i];
+      KeyState new_state = static_cast<KeyState>(in_val);
 
-      if (status.state == in_val)
+      if (status.state == new_state)
         continue;
 
       unsigned long time_now = millis();
       if ( (time_now - status.last_change) < debounce_ms )
         continue;
 
-      if (status.state == KEY_STATE_UP && in_val == KEY_STATE_DOWN){
+      if (status.state == KeyState::UP && new_state == KeyState::DOWN){
         Serial.println("Key UP -> DOWN");
         //set key in keyreport
         for (size_t key_i = 0; key_i < dim(report.keys); key_i++){
           if (report.keys[key_i] == KEY_REPORT_KEY_AVAILABLE){
             KeySym key_sym = symbol_table[layer][in_i][out_i];
-            if (key_sym.is_key){
+            switch (key_sym.type){
+            case KeyType::KEY:
               report.keys[key_i] = key_sym.val.key;
               status.report_key_i = key_i;
+              break;
+            case KeyType::MOD:
+              break;
+            case KeyType::FUN:
+              break;
+            default:
+              ;//SHOULD ONLY HAPPEN WITH INVALID SYMBOL_TABLE
             }
+
             status.last_change = time_now;
-            status.state = KEY_STATE_DOWN;
+            status.state = KeyState::DOWN;
             has_keys_changed = true;
 
             DEBUGV(key_i);
@@ -155,19 +179,19 @@ void loop()
           }
         }
       }
-      else if (status.state == KEY_STATE_DOWN && in_val == KEY_STATE_UP){
+      else if (status.state == KeyState::DOWN && new_state == KeyState::UP){
         Serial.println("Key DOWN -> UP");
         size_t clear_key_i = status.report_key_i;
         if ( clear_key_i < dim(report.keys)){
           report.keys[clear_key_i] = KEY_REPORT_KEY_AVAILABLE;
           status.last_change = time_now;
           has_keys_changed = true;
-          status.state = KEY_STATE_UP;
+          status.state = KeyState::UP;
 
           DEBUGV(status.report_key_i);
         }
         else{
-          //ASSERT SHOULD NOT HAPPEN => ERROR IN CODE
+          //SHOULD NOT HAPPEN => ERROR IN CODE
         }
       }
     }
