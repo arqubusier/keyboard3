f7dee2e0f9e31046512cbd6c0a6fb13dfb578801
Herman Lundkvist <herlu184@student.liu.se>
2018-10-23 22:21:23 +0200

    Created KeyboardState struct, worked a bit on layers.

--- keyboard3.ino
+++ keyboard3.ino
@@ -37,34 +37,36 @@ void print_array(T(& arr)[N])
 const uint8_t KEY_REPORT_KEY_AVAILABLE = 0;
 enum struct KeyInValue {UP=HIGH, DOWN=LOW};
 enum struct KeyState {KEY_DOWN, MOD_DOWN, FUN_DOWN, UP};
-union KeyIndex{
-  uint8_t report_key;
-  uint8_t mod_mask;
-  uint8_t fun_val;
-};
 
 const uint8_t FUN_PREFIX_MASK = 0xC0;
 const uint8_t FUN_SUB_MASK = 0x3F;
-const uint8_t FUN_LAYER=0x00;
-const uint8_t FUN_LAYER_OFFSET=0x04;
+const uint8_t FUN_LOFFS=0x00;
+const uint8_t FUN_LBASE=0x04;
 const uint8_t FUN_MACRO=0x80;
 const uint8_t FUN_OTHER=0xC0;
 
-union KeyVal{
-  byte key;
-  byte mod;
-  byte fun;
+struct KeyboardState{
+  static const size_t N_LAYERS = 64;
+  uint8_t layer_base;
+  uint8_t layer_offset;
+  KeyReport report;
+  uint8_t layer(){
+    uint8_t layer=layer_base+layer_offset;
+    if (layer>N_LAYERS)
+      return 0;
+    return layer;
+  }
 };
 
 struct KeySym{
   KeyState effect;
-  KeyVal val;
+  byte val;
 };
 
 struct KeyStatus{
   KeyState state;
   unsigned long last_change;
-  KeyIndex index;
+  uint8_t reset_val;
 
   bool isUp(){return state == KeyState::UP;}
   bool isDown(){return state != KeyState::UP;}
@@ -77,43 +79,25 @@ struct KeyStatus{
   void key_down(KeySym key_sym, KeyReport &report){
     for (size_t key_i = 0; key_i < dim(report.keys); key_i++){
       if (report.keys[key_i] == KEY_REPORT_KEY_AVAILABLE){
-        report.keys[key_i] = key_sym.val.key;
-        this->index.report_key = key_i;
-        this->state = key_sym.effect;
-        DEBUGV(key_i);
-        DEBUGV(key_sym.val.key);
+        report.keys[key_i] = key_sym.val;
+        reset_val = key_i;
+        state = key_sym.effect;
         return;
       }
     }
     this->state = KeyState::UP;
   }
 
-  void fun_down(byte fun_val, uint8_t &layer){
+  void fun_down(byte fun_val, KeyboardState &keyboard){
     auto fun_content = fun_val & FUN_SUB_MASK;
-    index.fun_val = fun_content;
+    reset_val = 0;
     switch (FUN_PREFIX_MASK & fun_val){
-    case FUN_LAYER:
-      layer = fun_content;
+    case FUN_LOFFS:
+      reset_val = keyboard.layer_offset;
+      keyboard.layer_offset = fun_content;
+      Serial.println("LayerOffset Down");
       break;
-    case FUN_LAYER_OFFSET:
-      break;
-    case FUN_MACRO:
-      break;
-    case FUN_OTHER:
-      break;
-    default:
-      ;
-    }
-    this->state = KeyState::FUN_DOWN;
-  }
-
-  void fun_up(uint8_t &layer){
-    switch (FUN_PREFIX_MASK & index.fun_val){
-    case FUN_LAYER:
-      index.fun_val = 0;
-      layer = 0;
-      break;
-    case FUN_LAYER_OFFSET:
+    case FUN_LBASE:
       break;
     case FUN_MACRO:
       break;
@@ -125,22 +109,45 @@ struct KeyStatus{
     this->state = KeyState::FUN_DOWN;
   }
 
-  bool up2down(KeySym key_sym, unsigned long time_up, KeyReport &report, uint8_t &layer){
+  void fun_up(KeyboardState &keyboard){
+    uint8_t old_layer=0;
+    auto fun_content = reset_val & FUN_SUB_MASK;
+    switch (FUN_PREFIX_MASK & reset_val){
+    case FUN_LOFFS:
+      old_layer = FUN_SUB_MASK & fun_content;
+      reset_val = 0;
+      keyboard.layer_offset = old_layer;
+      Serial.println("LayerOffset UP");
+      break;
+    case FUN_LBASE:
+      break;
+    case FUN_MACRO:
+      break;
+    case FUN_OTHER:
+      break;
+    default:
+      ;
+    }
+    this->state = KeyState::FUN_DOWN;
+  }
+
+  bool up2down(KeySym key_sym, unsigned long time_up, KeyboardState &keyboard){
     Serial.println("Key UP -> DOWN");
     switch (key_sym.effect){
     case KeyState::KEY_DOWN:
-      key_down(key_sym, report);
+      key_down(key_sym, keyboard.report);
+      DEBUGV(reset_val);
+      DEBUGV(key_sym.val);
       break;
     case KeyState::MOD_DOWN:
-      report.modifiers |= key_sym.val.mod;
-      this->index.mod_mask = key_sym.val.mod;
-      this->state = key_sym.effect;
-      DEBUGV(report.modifiers);
+      keyboard.report.modifiers |= key_sym.val;
+      reset_val = key_sym.val;
+      state = key_sym.effect;
+      DEBUGV(keyboard.report.modifiers);
       break;
     case KeyState::FUN_DOWN:
-      fun_down(key_sym.val.fun, layer);
-      DEBUGV(key_sym.val.fun);
-      DEBUGV(layer);
+      fun_down(key_sym.val, keyboard);
+      DEBUGV(keyboard.layer_offset);
       break;
     default:
       this->state = KeyState::UP;
@@ -154,25 +161,23 @@ struct KeyStatus{
     return false;
   }
 
-  bool down2up(unsigned long time_down, KeyReport &report, uint8_t &layer){
+  bool down2up(unsigned long time_down, KeyboardState &keyboard){
     Serial.println("Key DOWN -> UP");
     uint8_t clear_index;
     switch (state){
     case KeyState::KEY_DOWN:
-      clear_index = index.report_key;
+      clear_index = reset_val;
       // assert( clear_key_i < dim(report.keys))
-      report.keys[clear_index] = KEY_REPORT_KEY_AVAILABLE;
-      DEBUGV(index.report_key);
+      keyboard.report.keys[clear_index] = KEY_REPORT_KEY_AVAILABLE;
+      DEBUGV(reset_val);
       break;
     case KeyState::MOD_DOWN:
-      report.modifiers &= ~(index.mod_mask);
-      DEBUGV(index.mod_mask);
-      DEBUGV(~index.mod_mask);
-      DEBUGV(report.modifiers);
+      keyboard.report.modifiers &= ~(reset_val);
+      DEBUGV(keyboard.report.modifiers);
       break;
     case KeyState::FUN_DOWN:
-      fun_up(layer);
-      DEBUGV(layer);
+      fun_up(keyboard);
+      DEBUGV(keyboard.layer_offset);
     default:
       ;
     }
@@ -199,24 +204,31 @@ const int OUT_PINS[]  {10, 16};
 const unsigned long DEBOUNCE_MS = 5;
 const uint8_t KEY_REPORT_DONT_CLEAR_KEY = 255;
 
-const size_t LAYERS = 64;
-uint8_t layer = 0;
+KeyboardState keyboard_state;
 KeyStatus status_table[dim(IN_PINS)][dim(OUT_PINS)];
 //NOTE: for keymaps with duplicate keys, duplicate key reports may be sent.
 #define EMPTY_ROW { {{KeyState::UP, 0}, {KeyState::UP, 0}}, {{KeyState::UP, 0}, {KeyState::UP, 0}} }
-const KeySym SYMBOL_TABLE[LAYERS][dim(IN_PINS)][dim(OUT_PINS)] =
+#define K_UP KeyState::UP
+#define K_DN KeyState::KEY_DOWN
+#define M_DN KeyState::MOD_DOWN
+#define F_DN KeyState::FUN_DOWN
+#define F_LO FUN_LOFFS
+#define F_LB FUN_LBASE
+#define F_MO FUN_MACRO
+#define F_OT FUN_OTHER
+  const KeySym SYMBOL_TABLE[KeyboardState::N_LAYERS][dim(IN_PINS)][dim(OUT_PINS)] =
   {
    {
-    {{KeyState::MOD_DOWN, MOD_LSHIFT}, {KeyState::FUN_DOWN, 0x01}},
-    {{KeyState::KEY_DOWN, KEY_SW_C},   {KeyState::KEY_DOWN, KEY_SW_D}},
+    {{M_DN, MOD_LSHIFT}, {F_DN, 0x01}},
+    {{K_DN, KEY_SW_C  }, {K_DN, KEY_SW_D }},
    },
    {
-    {{KeyState::MOD_DOWN, KEY_SW_A},           {KeyState::MOD_DOWN, MOD_LCTRL}},
-    {{KeyState::FUN_DOWN, 0x02},   {KeyState::KEY_DOWN, KEY_SW_D}},
+    {{K_DN, KEY_SW_A  }, {F_DN, F_LO&0x01}},
+    {{F_DN, 0x02 }, {K_DN, KEY_SW_B }},
    },
    {
-    {{KeyState::MOD_DOWN, KEY_SW_F}, {KeyState::MOD_DOWN, MOD_LCTRL}},
-    {{KeyState::KEY_DOWN, KEY_SW_C}, {KeyState::KEY_DOWN, KEY_SW_D}},
+    {{M_DN, MOD_LCTRL }, {F_DN, F_LO&0x01}},
+    {{F_DN, F_LO&0x02 }, {K_DN, KEY_SW_E }},
    },
    EMPTY_ROW,
    EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW,
@@ -232,12 +244,22 @@ const KeySym SYMBOL_TABLE[LAYERS][dim(IN_PINS)][dim(OUT_PINS)] =
    EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW,
    EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW, EMPTY_ROW,
   };
+#undef K_UP
+#undef K_DN
+#undef M_DN
+#undef F_DN
+#undef F_LO
+#undef F_LB
+#undef F_MO
+#undef F_OT
 
 
 void setup()
 {
+  keyboard_state.layer_base = 0;
+  keyboard_state.layer_offset = 0;
   for (size_t i = 0; i < dim(report.keys); i++){
-    report.keys[i] = KEY_REPORT_KEY_AVAILABLE;
+    keyboard_state.report.keys[i] = KEY_REPORT_KEY_AVAILABLE;
   }
 
   for (size_t out_i = 0; out_i < dim(OUT_PINS); out_i++){
@@ -295,12 +317,13 @@ void loop()
         continue;
 
       if (key_status.isUp() && read_val == KeyInValue::DOWN){
-        KeySym key_sym = SYMBOL_TABLE[layer][in_i][out_i];
-        if (key_status.up2down(key_sym, time_now, report, layer))
+        KeySym key_sym = SYMBOL_TABLE[keyboard_state.layer()][in_i][out_i];
+        DEBUGV(keyboard_state.layer());
+        if (key_status.up2down(key_sym, time_now, keyboard_state))
           notify_key_change = true;
       }
       else if (key_status.isDown() && read_val == KeyInValue::UP){
-        if (key_status.down2up(time_now, report, layer))
+        if (key_status.down2up(time_now, keyboard_state))
           notify_key_change = true;
       }
     }
